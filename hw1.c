839c31e5a9f40981bc293f6b037629db638d510f
fierceblizzard <fierceblizzard11@gmail.com>
2023-03-02 18:36:16 -0500

    needed to fix memory leaks

--- hw1.c
+++ hw1.c
@@ -29,32 +29,39 @@ void matrixArrayOfArrays(int n, int m){
         }
         printf("\n");
     }
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < m; i++){
         free(mat[i]);
     }
+    free(mat);
 }
 
 void matrixOneBigArray(int n, int m){
     float* mat = malloc((n * m) * sizeof(float));
     float num = 1.0;
 
+    float** matBig  = (float**)malloc(m * sizeof(float*));
+
     for(int i = 0; i < m * n; i++){
         mat[i] = num;
         num += 1.0;
     }
+    for(int i = 0; i < n; i++){
+        matBig[i] = &mat[i*m];
+    }
     for(int i = 0; i < m; i++){
         for(int t = 0; t < n; t++){
-            printf("%f\t", mat[t * m + i]);
+            printf("%f\t", matBig[t][i]);
         }
         printf("\n");
     }
     for(int i = 0; i < n; i++){
         for(int t = 0; t < m; t++){
-            printf("%f\t", mat[i * m + t]);
+            printf("%f\t", matBig[i][t]);
         }
         printf("\n");
     }
     free(mat);
+    free(matBig);
 }
 
 int main(int argc, char** argv){
